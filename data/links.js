cce639bcdb8b772583cb8ac6c9e45b29db200213
Анастасия Мамаева <anastasiamamaeva@MacBook-Air-Anastasia.local>
2023-08-02 08:11:35 +0300

    add lots of links

---
+++ data/links.js
@@ -0,0 +1,441 @@
+export const links = {
+    caniuse: {
+        name: 'CanIUse',
+        link: 'https://caniuse.com/',
+        description: 'проверка совместимости с браузером',
+    },
+    lodash: {
+        name: 'Lodash',
+        link: 'https://lodash.com',
+        description: 'удобная популярная библиотека с широким функционалом',
+    },
+    mobilefriendly: {
+        name: 'MobileFriendly',
+        link: 'https://search.google.com/test/mobile-friendly',
+        description: 'проверить совместимость с мобильным браузером',
+    },
+    mdn: {
+        name: 'MDN',
+        link: 'https://developer.mozilla.org/ru/',
+        description: 'документация к веб-технологиям',
+    },
+    cssruler: {
+        name: 'CSS Ruler',
+        link: 'https://katydecorah.com/css-ruler/',
+        description: 'наглядное сравнение единиц измерения',
+    },
+    emmetcheatsheet: {
+        name: 'Шпаргалка Emmet',
+        link: 'https://dwstroy.ru/stail/plaginy-rasshireniya/emmet-shpargalka/',
+        description: '',
+    },
+    doka: {
+        name: 'Дока',
+        link: 'https://doka.guide',
+        description: 'красивый сайт от выпускников Яндекс.Практикума с документацией и советами',
+    },
+    cssstructurestyle: {
+        name: 'Стили организации CSS-кода',
+        link: 'https://habr.com/ru/articles/256109/',
+        description: 'подробная статья о способах организации кода',
+    },
+    htmlhint: {
+        name: 'HTMLHint',
+        link: 'https://htmlhint.com',
+        description: 'анализатор html-кода',
+    },
+    learnjs: {
+        name: 'Современный учебник JavaScript',
+        link: 'https://learn.javascript.ru',
+        description: 'интерактивный учебник',
+    },
+    csslive: {
+        name: 'css-live.ru',
+        link: 'https://css-live.ru/o-nas',
+        description: 'сетевой журнал о консервативной веб-разработке',
+    },
+    colorscheme: {
+        name: 'ColorScheme.ru',
+        link: 'https://colorscheme.ru/',
+        description: 'палитра для подбора цветов и контраста',
+    },
+    googlefonts: {
+        name: 'Google Fonts',
+        link: 'https://fonts.google.com/',
+        description: 'библиотека бесплатных шрифтов Google',
+    },
+    gridgarden: {
+        name: 'Grid Garden',
+        link: 'https://cssgridgarden.com/#ru',
+        description: 'упражнение для запоминания grid для детей и джунов',
+    },
+    sass: {
+        name: 'Sass',
+        link: 'https://sass-lang.su',
+        description: 'тот самый препроцессор CSS',
+    },
+    learnxinyminutes: {
+        name: 'Learn X in Y Minutes',
+        link: 'https://learnxinyminutes.com',
+        description: 'удобная шпаргалка по всем популярным языкам',
+    },
+    paratype: {
+        name: 'ParaType',
+        link: 'https://www.paratype.ru/',
+        description: 'российская шрифтовая компания',
+    },
+    htmlcolorcodes: {
+        name: 'html color codes',
+        link: 'https://htmlcolorcodes.com/',
+        description: 'наиболее полный сайт про цвет в вебе',
+    },
+    codepen: {
+        name: 'CodePen',
+        link: 'https://codepen.io/',
+        description: 'онлайн-песочница для фронтенд-проектов',
+    },
+    sassmeister: {
+        name: 'Sassmeister',
+        link: 'https://www.sassmeister.com/',
+        description: 'визуализация функционала препроцессора Sass',
+    },
+    bigocheatsheet: {
+        name: 'BigO cheatsheet',
+        link: 'https://www.bigocheatsheet.com',
+        description: 'шпаргалка алгоритмической сложности',
+    },
+    compatibilitytable: {
+        name: 'ECMAScript compatibility table',
+        link: 'https://kangax.github.io/compat-table/es6/',
+        description: 'совместимость функционала JS с браузерами и сборщиками',
+    },
+    assertru: {
+        name: 'Assert',
+        link: 'https://runebook.dev/ru/docs/node/assert',
+        description: 'документация к модулю assert Node.js на русском языке',
+    },
+    powerassert: {
+        name: 'Power Assert',
+        link: 'https://github.com/power-assert-js/power-assert',
+        description: 'расширение к модулю assert',
+    },
+    jest: {
+        name: 'Jest',
+        link: 'https://jestjs.io/ru/',
+        description: 'мощный фреймфорк для тестирования JS',
+    },
+    visualisecode: {
+        name: 'Python Tutor: Visualize code',
+        link: 'https://pythontutor.com/visualize.html#mode=edit',
+        description: 'визуализация исполнения кода',
+    },
+    nodejspackage: {
+        name: 'hexlet-boilerplates/nodejs-package',
+        link: 'https://github.com/hexlet-boilerplates/nodejs-package',
+        description: 'эталонный пакет на JS от Hexlet',
+    },
+    optimizationguide: {
+        name: 'Продуманная оптимизация',
+        link: 'https://optimization.guide',
+        description: 'серия статей про умную оптимизацию кода',
+    },
+    codewars: {
+        name: 'codewars',
+        link: 'https://www.codewars.com',
+        description: 'архив упражнений и кодинг на скорость',
+    },
+    codebattle: {
+        name: 'Codebattle',
+        link: 'https://codebattle.hexlet.io',
+        description: 'кодинг на скорость с реальным оппонентом или ботом',
+    },
+    rutestassignments: {
+        name: 'Список тестовых заданий для прокачки',
+        link: 'https://github.com/Hexlet/ru-test-assignments',
+        description: 'разные тестовые задания реальных компаний, собранные Хекслетом',
+    },
+    leetcode: {
+        name: 'LeetCode',
+        link: 'https://leetcode.com',
+        description: 'продвинутые упражнения для практики',
+    },
+    eslint: {
+        name: 'ESLint',
+        link: 'https://eslint.org/',
+        description: 'лучший и единственный динтер для JS',
+    },
+    smacss: {
+        name: 'SMACSS',
+        link: 'https://smacss.com ',
+        description: 'стиль организации CSS-кода',
+    },
+    codesandbox: {
+        name: 'CodeSandbox',
+        link: 'https://codesandbox.io',
+        description: 'онлайн-песочница и редактор для планшетов под JS и его родственников',
+    },
+    emmetcheatsheet: {
+        name: '',
+        link: '',
+        description: '',
+    },
+    emmetcheatsheet: {
+        name: '',
+        link: '',
+        description: '',
+    },
+    emmetcheatsheet: {
+        name: '',
+        link: '',
+        description: '',
+    },
+    emmetcheatsheet: {
+        name: '',
+        link: '',
+        description: '',
+    },
+    emmetcheatsheet: {
+        name: '',
+        link: '',
+        description: '',
+    },
+    emmetcheatsheet: {
+        name: '',
+        link: '',
+        description: '',
+    },
+    emmetcheatsheet: {
+        name: '',
+        link: '',
+        description: '',
+    },
+    emmetcheatsheet: {
+        name: '',
+        link: '',
+        description: '',
+    },
+};
+
+export const lang = {
+    js: [
+        links.lodash,
+        links.mdn,
+        links.doka,
+        links.learnjs,
+        links.csslive,
+        links.learnxinyminutes,
+        links.codepen,
+        links.compatibilitytable,
+        links.assertru,
+        links.powerassert,
+        links.jest,
+        links.visualisecode,
+        links.nodejspackage,
+        links.codewars,
+        links.codebattle,
+        links.eslint,
+        links.codesandbox,
+    ],
+    html: [
+        links.mdn,
+        links.doka,
+        links.htmlhint,
+        links.csslive,
+        links.learnxinyminutes,
+        links.codepen,
+    ],
+    css: [
+        links.mdn,
+        links.cssruler,
+        links.doka,
+        links.cssstructurestyle,
+        links.csslive,
+        links.gridgarden,
+        links.sass,
+        links.learnxinyminutes,
+        links.codepen,
+        links.sassmeister,
+        links.smacss,
+    ],
+    python: [
+        links.visualisecode,
+        links.learnxinyminutes,
+        links.codewars,
+        links.codebattle,
+    ],
+    c: [
+        links.visualisecode,
+        links.learnxinyminutes,
+        links.codewars,
+    ],
+    csplus: [
+        links.visualisecode,
+        links.learnxinyminutes,
+        links.codewars,
+        links.codebattle,
+    ],
+    csharp: [
+        links.codebattle,
+        links.learnxinyminutes,
+    ],
+    java: [
+        links.visualisecode,
+        links.learnxinyminutes,
+        links.codewars,
+    ],
+    php: [
+        links.codewars,
+        links.codebattle,
+        links.learnxinyminutes,
+    ],
+    coffeescript: [
+        links.codewars,
+        links.codebattle,
+        links.learnxinyminutes,
+    ],
+    typescript: [
+        links.codewars,
+        links.codebattle,
+        links.learnxinyminutes,
+        links.codesandbox,
+    ],
+    ruby: [
+        links.codewars,
+        links.codebattle,
+        links.learnxinyminutes,
+    ],
+    haskell: [
+        links.codewars,
+        links.codebattle,
+        links.learnxinyminutes,
+    ],
+    kotlin: [
+        links.codewars,
+        links.codebattle,
+        links.learnxinyminutes,
+    ],
+    go: [
+        links.codewars,
+        links.codebattle,
+        links.learnxinyminutes,
+    ],
+    elixir: [
+        links.codewars,
+        links.codebattle,
+        links.learnxinyminutes,
+    ],
+    clojure: [
+        links.codewars,
+        links.codebattle,
+        links.learnxinyminutes,
+    ],
+    dart: [
+        links.codewars,
+        links.codebattle,
+        links.learnxinyminutes,
+    ],
+    erlang: [
+        links.codewars,
+        links.codebattle,
+        links.learnxinyminutes,
+    ],
+};
+
+export const type = {
+    lib: [
+        links.lodash,
+    ],
+    framework: [
+        links.jest,
+    ],
+    docs: [
+        links.lodash,
+        links.mdn,
+        links.doka,
+        links.sass,
+        links.assertru,
+        links.powerassert,
+        links.jest,
+    ],
+    studybook: [
+        links.mdn,
+        links.learnjs,
+        links.optimizationguide,
+    ],
+    cheatsheet: [
+        links.emmetcheatsheet,
+        links.learnxinyminutes,
+        links.bigocheatsheet,
+    ],
+    article: [
+        links.cssstructurestyle,
+    ],
+    linter: [
+        links.htmlhint,
+        links.eslint,
+    ],
+    magazin: [
+        links.csslive,
+    ],
+    practice: [
+        links.gridgarden,
+        links.sassmeister,
+        links.codewars,
+        links.codebattle,
+        links.rutestassignments,
+        links.leetcode,
+    ],
+    editor: [
+        links.codepen,
+        links.codesandbox,
+    ],
+    example: [
+        links.nodejspackage,
+    ],
+};
+
+export const issue = {
+    compatibility: [
+        links.caniuse,
+        links.mobilefriendly,
+        links.compatibilitytable,
+    ],
+    web: [
+        links.caniuse,
+        links.mobilefriendly,
+        links.mdn,
+        links.doka,
+        links.csslive,
+        links.codepen,
+        links.compatibilitytable,
+    ],
+    mobile: [
+        links.mobilefriendly,
+    ],
+    ecosystem: [
+        links.emmetcheatsheet,
+    ],
+    color: [
+        links.colorscheme,
+        links.htmlcolorcodes,
+    ],
+    font: [
+        links.googlefonts,
+        links.paratype,
+    ],
+    algorythm: [
+        links.bigocheatsheet,
+    ],
+    test: [
+        links.assertru,
+        links.powerassert,
+        links.jest,
+        links.visualisecode,
+    ],
+    dependencies: [
+        links.nodejspackage,
+    ],
+    optimization: [
+        links.optimizationguide,
+    ],
+};
