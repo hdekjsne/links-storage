74a0ba8be412a6753cb8ed322288ed87d706a13c
Анастасия Мамаева <anastasiamamaeva@MacBook-Air-Anastasia.local>
2023-08-04 02:57:55 +0300

    correct links cleaning

--- script.js
+++ script.js
@@ -1,11 +1,18 @@
-import { links, lang, type, issue } from './data/links.js';
+import { lang, type, issue } from './data/links.js';
 
 const chosenContainer = document.getElementById('chosen');
+const linksContainer = document.getElementById('links-container');
 
 let chosen = [];
 let chosenInfo = [];
 
-function makeLinks () {
+function clearLinks() {
+    while (linksContainer.childNodes.length > 0) {
+        linksContainer.childNodes[0].remove();
+    }
+}
+
+function makeLinks() {
     const preprocessed = [];
     for (const [section, id] of chosenInfo) {
         switch (section) {
@@ -22,7 +29,7 @@ function makeLinks () {
                 console.log('something\'s wrong:(');
         }
     }
-    console.log(preprocessed);
+    if (preprocessed.length === 0) return;
     const processed = preprocessed[0]
         .filter((item) => {
             const booleans = preprocessed.map((links) => {
@@ -30,9 +37,19 @@ function makeLinks () {
             });
             return booleans.includes(false) ? false : true;
         });
+    processed.map((link) => {
+        const element = document.createElement('p');
+        element.innerHTML = `<a href='${link.link}'>${link.name}</a> : ${link.description}`;
+        element.style.width = '100%';
+        element.style.padding = '10px';
+        element.style.verticalAlign = 'center';
+        element.classList.add('reset');
+        linksContainer.prepend(element);
+    });
 }
 
-function addNewTag (event) {
+function addNewTag(event) {
+    // clearLinks();
     const target = event.target;
     const section = target.dataset.section; // lang / type / issue
     const id = target.dataset.lang ?? target.dataset.type ?? target.dataset.issue;
@@ -46,18 +63,21 @@ function addNewTag (event) {
     makeLinks();
 }
 
-function removeTag (event) {
+function removeTag(event) {
+    // clearLinks();
     const target = event.target;
     const index = chosen.indexOf(target);
     chosen = chosen.filter((el) => el !== target);
     chosenInfo = chosenInfo
         .filter((el, elIndex) => elIndex !== index);
     if (target instanceof HTMLParagraphElement) target.remove();
+    makeLinks();
 }
 
+chosenContainer.addEventListener('click', clearLinks);
 const tags = document.getElementsByClassName('tag');
-
 for (const tag of tags) {
+    tag.addEventListener('click', clearLinks);
     tag.addEventListener('click', addNewTag);
 }
 chosenContainer.addEventListener('click', removeTag);
