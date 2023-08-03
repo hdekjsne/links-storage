186ff203b41d3f4cc867f1aead54d00d849176d2
Анастасия Мамаева <anastasiamamaeva@MacBook-Air-Anastasia.local>
2023-08-03 16:14:55 +0300

    plus two fuctions

--- script.js
+++ script.js
@@ -1,10 +1,31 @@
-const chosen = [];
+export { links, lang, type, issue } from './data/links.js';
 
-function addNewTag (target) {
+const chosenContainer = document.getElementById('chosen');
+
+let chosen = [];
+
+function addNewTag (event) {
+    const target = event.target;
+    const section = target.dataset.section;
     const id = target.dataset.lang ?? target.dataset.type ?? target.dataset.issue;
-    const section = 
+    const element = document.createElement('p');
+    element.textContent = target.textContent;
+    element.style.fontSize = '18px';
+    element.style.padding = '3px 10px';
+    chosen.push(element);
+    chosenContainer.prepend(element);
 }
 
-document.getElementsByClassName('tag').forEach((tag) => {
-    tag.addEventListener('click', );
-})
+function removeTag (event) {
+    const target = event.target;
+    console.log(target);
+    chosen = chosen.filter((el) => el !== target);
+    target.remove();
+}
+
+const tags = document.getElementsByClassName('tag');
+
+for (const tag of tags) {
+    tag.addEventListener('click', addNewTag);
+}
+chosenContainer.addEventListener('click', removeTag);
