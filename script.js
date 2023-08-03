16ce3d91f60e83e3d6f5c4c5e3c4caa004083da0
Анастасия Мамаева <anastasiamamaeva@MacBook-Air-Anastasia.local>
2023-08-03 15:42:42 +0300

    add more data selectors

---
+++ script.js
@@ -0,0 +1,10 @@
+const chosen = [];
+
+function addNewTag (target) {
+    const id = target.dataset.lang ?? target.dataset.type ?? target.dataset.issue;
+    const section = 
+}
+
+document.getElementsByClassName('tag').forEach((tag) => {
+    tag.addEventListener('click', );
+})
